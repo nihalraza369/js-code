63af0303af9e390f8d7f48ba6b9bcc6e7858eb71
nihalraza369 <nihalraza369@gmail.com>
2024-12-07 14:52:33 +0500

    initailize reposity

---
+++ index6.js
@@ -0,0 +1,33 @@
+let mydate =new Date()
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());
+console.log(mydate.toTimeString());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toUTCString());
+console.log(mydate.getDate());
+console.log(mydate.getDay());
+console.log(mydate.getHours());
+console.log(mydate.getFullYear());
+console.log(mydate.getMilliseconds());
+console.log(mydate.getMinutes());
+console.log(mydate.getMonth());
+console.log(mydate.getTime());
+console.log(mydate.getTimezoneOffset());
+console.log(mydate.getUTCDate());
+console.log(mydate.getUTCDay());
+console.log(mydate.getFullYear());
+console.log(typeof mydate);
+
+
+
+let mydaten=new Date(2024,10,6)
+console.log(mydaten.toDateString());
+
+
+let mydatene=new Date(2024,10,6)
+console.log(mydatene.toLocaleString());
+
